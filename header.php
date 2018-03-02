17cdcc19d094986aafd9d2f26c57dc1663ac1788
Thiago <souza.ty@gmail.com>
2018-03-02 16:46:31 -0300

    Update Header
    Add Hotjatr tracking code

--- header.php
+++ header.php
@@ -27,6 +27,17 @@
 
           gtag('config', 'UA-115028585-1');
         </script>
+        <!-- Hotjar Tracking Code for http://heelj.org.br -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:800557,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
 	</head>
 	<body <?php body_class(); ?> >
         <!-- ### Menu Principal ### -->
