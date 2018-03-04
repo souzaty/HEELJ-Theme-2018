e921b0755638c2f922e3beeae1208d28f336dadb
Thiago <souza.ty@gmail.com>
2018-03-04 18:52:36 -0300

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -16,25 +16,3 @@
     	<input class="submit-404" type="submit" id="searchsubmit" value="Ir" />
 	</div>
 </form>
-
-<style type="text/css">
-.input-404 {
-padding: 10px 20px;
-border: 1px solid #333;
-font-size: 1.1em;
-margin: 0;
-border-right: 0;
-height: 45px;
-width: 300px;
-float: left;
-}
-.submit-404 {
-height: 45px;
-border:1px solid #000;
-background: #000;
-color: #fff;
-padding: 20px;
-margin: 0;
-float: left;
-}
-</style>
